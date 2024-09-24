7286ed91101a8a501528395fdd444f05b13cc302
Jasim Uddin <jasimbdpro@gmail.com>
2024-09-24 21:34:04 +0600

    nw

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 const Home = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
-            <h1>5</h1>
+            <h1>6</h1>
+            <Link to='/test'>Test</Link>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
