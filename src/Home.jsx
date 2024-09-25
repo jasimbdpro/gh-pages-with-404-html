7b2054969e3c0423ce09798c6fd10e18686ba11a
Jasim Uddin <jasimbdpro@gmail.com>
2024-09-25 12:01:18 +0600

    nw

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     const [count, setCount] = useState(0)
     return (
         <div>
-            <h1>7s</h1>
+            <h1>8</h1>
             <Link to='/test'>Test</Link>
             <div className="card">
                 <button onClick={() => setCount((count) => count + 1)}>
